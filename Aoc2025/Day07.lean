aff18463c5a40da89e0196674ea7d37c171d6358
eutro <git@eutro.dev>
2025-12-07 09:15:30 +0000

    Clean up day 7

--- Aoc2025/Day07.lean
+++ Aoc2025/Day07.lean
@@ -31,47 +31,32 @@ def parseInput : IO (Coord × Grid Cell) := do
     for x in List.range grid.width do
       let pos := Coord.mk x y
       if grid[pos]? = .some .start
-      then start := .some pos
-           break
+      then start := .some pos ; break
   return (start.get!, grid)
 
 def Std.HashMap.insertFold {α β : Type} [BEq α] [Hashable α] 
   (map : Std.HashMap α β) (key : α) (val : β) (fold : β -> β -> β) : Std.HashMap α β :=
-  map.alter key (fun
+  map.alter key fun
     | .none => val
-    | .some val' => fold val val')
+    | .some val' => fold val val'
 
-def countBeams {α : Type}
-  (start : Coord) (grid : Grid Cell)
-  (init : α) (fold : α -> α -> α) : Nat × List α := Id.run do
+def countBeams (start : Coord) (grid : Grid Cell) : Nat × Nat := Id.run do
   let mut splits := 0
-  let mut xs : Std.HashMap Nat α := Std.HashMap.emptyWithCapacity
-    |>.insert start.x.toNat init
+  let mut xs : Std.HashMap Nat Nat := {(start.x.toNat, 1)}
   for y in List.inclusiveRange start.y.toNat grid.height do
-    let mut xs' := Std.HashMap.emptyWithCapacity
     for (x, fx) in xs do
-      let pos := Coord.mk x y
-      if IsSplitter grid pos then
+      if IsSplitter grid ⟨x, y⟩ then
         splits := splits + 1
-        xs' := xs'.insertFold (x - 1) fx fold
-        xs' := xs'.insertFold (x + 1) fx fold
-      else
-        xs' := xs'.insertFold x fx fold
-    xs := xs'
-  return (splits, xs.values)
-
-def part1 (start : Coord) (grid : Grid Cell) : Nat :=
-  let (count, _) := countBeams start grid 1 (· + ·)
-  count
-
-def part2 (start : Coord) (grid : Grid Cell) : Nat :=
-  let (_, worlds) := countBeams start grid 1 (· + ·)
-  worlds.sum
+        xs := xs.erase x
+        xs := xs.insertFold (x - 1) fx (· + ·)
+        xs := xs.insertFold (x + 1) fx (· + ·)
+  return (splits, xs.values.sum)
 
 end Day07
 open Day07
 
 def main : IO Unit := do
   let (start, grid) <- parseInput
-  IO.println s!"Part 1: {part1 start grid}"
-  IO.println s!"Part 2: {part2 start grid}"
+  let (part1, part2) := countBeams start grid
+  IO.println s!"Part 1: {part1}"
+  IO.println s!"Part 2: {part2}"
